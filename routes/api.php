43caa14cdb94d69e21ac359989b577a65e78d385
NovalRaihan <novalraihannoval@gmail.com>
2024-02-16 15:03:51 +0700

    test

--- routes/api.php
+++ routes/api.php
@@ -16,15 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group([
-
     'middleware' => 'api',
     'prefix' => 'auth'
-
 ], function ($router) {
-
     Route::post('login', [AuthController::class, 'login']);
-    // Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('me', [AuthController::class, 'me']);
-
 });
