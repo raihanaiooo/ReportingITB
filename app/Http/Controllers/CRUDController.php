6a0fe3985636f7f795d1eaa1b970d0e1534868c6
raihaniooo <raihanaaaz2@gmail.com>
2024-03-08 11:04:52 +0700

    update adobe berhasil

--- app/Http/Controllers/CRUDController.php
+++ app/Http/Controllers/CRUDController.php
@@ -11,36 +11,31 @@ use Illuminate\Http\Request;
 class CRUDController extends Controller
 {
     public function updateApi(Request $request)
-    {
+{
+    try {
+        $request->validate([
+            'used' => 'required|integer',
+        ]);
 
-        try{
-            $request->validate([
-                'total' => 'required|integer',
-                'used' => 'required|integer',
-                'app_type_id' => 'required|exists:apps,id',
-            ]);
-        
-            $license = Licenses::findOrFail($request->input('license_id'));
-        
-            // Hitung nilai available sebagai selisih antara total dan used
-            $available = $request->input('total') - $request->input('used');
-        
-            $license->update([
-                'total' => $request->input('total'),
-                'used' => $request->input('used'),
-                'available' => $available,
-                'app_type_id' => $request->input('app_type_id'),
-                'inserted_at' => Carbon::now(), // Set inserted_at ke waktu saat ini
-            ]);
-            
-            Log::info('License updated successfully');
+        // Find the license based on app_type_id equal to 3
+        $license = Licenses::where('app_type_id', 3)->firstOrFail();
 
-            return response()->json(['success' => 'License updated successfully'], 200);
+        // Calculate available as the difference between total and used
+        $available = $license->total - $request->input('used');
 
-        }catch(\Exception $e){
-            Log::error($e->getMessage());
+        $license->update([
+            'used' => $request->input('used'),
+            'available' => $available,
+            'inserted_at' => Carbon::now(), // Set inserted_at to the current time
+        ]);
+
+        Log::info('License updated successfully');
+
+        return response()->json(['success' => 'License updated successfully'], 200);
+    } catch (\Exception $e) {
+        Log::error($e->getMessage());
         return response()->json(['error' => 'Internal Server Error'], 500);
-        }
     }
+}
 
 }
